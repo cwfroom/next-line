6de82906d773086dbe27f235ed98156ac5717413
Wenfei Cao <wenfei@amanita.co.jp>
2022-04-25 12:12:56 +0900

    Combine deleteUntil

--- src/extension.ts
+++ src/extension.ts
@@ -2,51 +2,89 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+const terminateSymbols = [
+	"」",
+	"』",
+	"。",
+	"？",
+	"！",
+	"…",
+	"～",
+	"♪",
+	"）"
+];
+
+function deleteUntil () {
+	const activeEditor = vscode.window.activeTextEditor;
+	if (activeEditor) {
+		const cursorPosition = activeEditor.selection.active;
+		const activeLine = activeEditor.document.lineAt(cursorPosition.line);
+		if (activeLine.text.length === 0) {return; };
+		let endChar = activeLine.text.length - 1;
+		// Iterate until passing one of the terminate symbols
+		for (endChar; endChar > cursorPosition.character; endChar--) {
+			if (!terminateSymbols.includes(activeLine.text.charAt(endChar))) {
+				endChar++;
+				break;
+			}
+		}
+		var endPosition = cursorPosition.with(cursorPosition.line, endChar);
+		var removeSelection = new vscode.Selection(cursorPosition,endPosition);
+		activeEditor.edit( builder => {
+		builder.delete(removeSelection);
+		});
+	}
+}
+
+async function nextLine() {
+	deleteUntil();
+	const activeEditor = vscode.window.activeTextEditor;
+	if (activeEditor) {
+		const cursorPosition = activeEditor.selection.active;
+		let lineOffset = 0;
+		for (let i = 1; i < 21; i++) {
+			const line = activeEditor.document.lineAt(cursorPosition.line + i);
+			if (line && line.text.length !== 0 && line.text[0] !== '/' && line.text[0] !== '@') {
+				lineOffset = i;
+				break;
+			}
+		}
+		if (lineOffset > 0) {
+			await vscode.commands.executeCommand('cursorMove', {
+				to: 'down',
+				by: 'wrappedLine',
+				value: lineOffset
+			});
+			await vscode.commands.executeCommand('cursorMove', {
+				to: 'wrappedLineStart',
+				by: 'wrappedLine',
+			});
+			const lineAfterMove = activeEditor.document.lineAt(cursorPosition.line + lineOffset);
+			if (lineAfterMove.text[0] === '「') {
+				await vscode.commands.executeCommand('cursorMove', {
+					to: 'right',
+					by: 'character'
+				});
+			}
+			await vscode.commands.executeCommand('editorScroll', {
+				to: 'down',
+				by: 'wrappedLine',
+				value: lineOffset
+			});
+		}
+	}
+}
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export async function activate(context: vscode.ExtensionContext) {
+export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.nextLine', async () => {
-		const activeEditor = vscode.window.activeTextEditor;
-		if (activeEditor) {
-			const cursorPosition = activeEditor.selection.active;
-			let lineOffset = 0;
-			for (let i = 1; i < 21; i++) {
-				const line = activeEditor.document.lineAt(cursorPosition.line + i);
-				if (line && line.text.length !== 0 && line.text[0] !== '/' ) {
-					lineOffset = i;
-					break;
-				}
-			}
-			if (lineOffset > 0) {
-				await vscode.commands.executeCommand('cursorMove', {
-					to: 'down',
-					by: 'wrappedLine',
-					value: lineOffset
-				});
-				await vscode.commands.executeCommand('cursorMove', {
-					to: 'wrappedLineStart',
-					by: 'wrappedLine',
-				});
-				const lineAfterMove = activeEditor.document.lineAt(cursorPosition.line + lineOffset);
-				if (lineAfterMove.text[0] === '「') {
-					await vscode.commands.executeCommand('cursorMove', {
-						to: 'right',
-						by: 'character'
-					});
-				}
-				await vscode.commands.executeCommand('editorScroll', {
-					to: 'down',
-					by: 'wrappedLine',
-					value: lineOffset
-				});
-			}
-		}
-	});
-
-	context.subscriptions.push(disposable);
+	let deleteUntilDisposable = vscode.commands.registerCommand('extension.deleteUntil', deleteUntil);
+	let nextLineDisposable = vscode.commands.registerCommand('extension.nextLine', nextLine);
+	context.subscriptions.push(deleteUntilDisposable);
+	context.subscriptions.push(nextLineDisposable);
 }
 
 // this method is called when your extension is deactivated
